d25a6d644d54dc623b6229d463ad4831fed757a9
Shubham Sehra <sehra.shubham11@gmail.com>
2021-10-12 01:11:35 +0530

    working on Array to work

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+
+const app = express();
+app.set("view engine","ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+let items = [];
+
+app.get("/", function(req, res){
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+  let day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {dayAndTime:day, itemsName: items});
+});
+
+app.post("/",function(req, res){
+  let name = req.body.item;
+  items.push(name);
+  res.redirect("/");
+})
+
+
+
+app.listen(3000,function(req,res){
+  console.log("Hello Shubham")
+});
